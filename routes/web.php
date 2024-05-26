7ab00180ab9bf4c0c53374d0e5dd020c52c02c5b
iibaaaadd <choirul.21059@mhs.unesa.ac.id>
2024-05-26 17:08:08 +0700

    sidebar

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+
+
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
 });
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
